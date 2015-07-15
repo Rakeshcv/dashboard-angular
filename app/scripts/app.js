324472f9d98746dae91665ee377d0502097902a6
Rakesh C V <rakesh.cv@hotmail.com>
2015-07-15 22:18:04 +1000

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name dashboardApp
+ * @description
+ * # dashboardApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('dashboardApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ]);
