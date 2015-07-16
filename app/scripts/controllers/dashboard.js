80872bcc9dc0b2df953b2c12652adb422dc1b016
Rakesh C V <rakesh.cv@hotmail.com>
2015-07-16 16:10:00 +1000

    Making site responsive

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -8,6 +8,37 @@
  * Controller of the dashboardApp
  */
 angular.module('dashboardApp')
-  .controller('DashboardCtrl',['$scope', function ($scope) {
+  .controller('DashboardCtrl',['$scope','$window','$timeout', function ($scope,$window,$timeout) {
 
+    $scope.$on('menu-route', function (event, data) {
+      $scope.routeString = data.route;
+    });
+
+
+    $($window).on('resize.dashboardButton', function () {
+      $scope.$apply(function () {
+
+        checkWidth();
+      })
+    });
+
+    $scope.$on('destroy', function () {
+      $($window).off('resize.dashboardButton');
+    });
+
+
+    function checkWidth() {
+      var width = $($window).innerWidth();
+      $scope.isMenuVisible = (width > 768);
+      $scope.isMenuButtonVisible = ! $scope.isMenuVisible;
+    }
+
+    $scope.menuButtonClicked = function(){
+      $scope.isMenuVisible = !$scope.isMenuVisible;
+      $scope.$apply();
+    };
+
+    $timeout(function () {
+      checkWidth();
+    },0);
   }]);
